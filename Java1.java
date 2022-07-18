6245325f51a7db9023c48d4bb177ff0518dcb695
Sahil Kumar <sahil1474.cse18@chitkara.edu.in>
2022-07-19 01:56:39 +0530

    1st Commit

---
+++ Java1.java
@@ -0,0 +1,12 @@
+
+
+public class Java1{
+    public static void main(String[] args)
+     {
+     
+System.out.println("Hello World\n");
+System.out.println("Let's have some fun while coding");
+
+    }
+
+}
